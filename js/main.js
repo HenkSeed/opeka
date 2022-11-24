c7ff753ccd6b3b941853d40eb0b0e3dee1aa9470
HenkSeed <adaner@inbox.ru>
2022-11-24 20:09:33 +0700

    Added responsive design and some JS code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-﻿
+﻿const regionSearch = document.querySelector('.region-search');
+const sexSearch = document.querySelector('.sex-search');
+const ageSearch = document.querySelector('.age-search');
+const selectArrow = document.querySelector('.select-arrow')
+
+const selectRegionArrow = document.getElementById('select-region__arrow')
+const selectSexArrow = document.getElementById('select-sex__arrow')
+const selectAgeArrow = document.getElementById('select-age__arrow')
+
+const toggleSearch = function () {
+	regionSearch.classList.toggle('hidden');
+   // regionSearch.
+};
+
+const toggleSexArrow = function () {
+	sexSearch.classList.toggle('hidden');
+   // regionSearch.
+};
+
+const toggleAgeArrow = function () {
+	ageSearch.classList.toggle('hidden');
+   // regionSearch.
+};
+
+regionSearch.addEventListener('click', toggleSearch);
+selectArrow.addEventListener('click', toggleSearch);
+
+selectSexArrow.addEventListener('click', toggleSexArrow);
+selectAgeArrow.addEventListener('click', toggleAgeArrow);
