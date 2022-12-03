1dd4aeb271f8d75cbd64c0b27b039f65f6d9949a
HenkSeed <adaner@inbox.ru>
2022-12-03 16:44:38 +0700

    Added burger menu and js code for it

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 ﻿const regionSearch = document.querySelector('.region-search');
 const sexSearch = document.querySelector('.sex-search');
 const ageSearch = document.querySelector('.age-search');
-const selectArrow = document.querySelector('.select-arrow')
+const selectArrow = document.querySelector('.select-arrow');
 
-const selectRegionArrow = document.getElementById('select-region__arrow')
-const selectSexArrow = document.getElementById('select-sex__arrow')
-const selectAgeArrow = document.getElementById('select-age__arrow')
+const selectRegionArrow = document.getElementById('select-region__arrow');
+const selectSexArrow = document.getElementById('select-sex__arrow');
+const selectAgeArrow = document.getElementById('select-age__arrow');
+
+const burgerIcon = document.querySelector('.burger__icon');
+const modalBurgerMenu = document.querySelector('.modal__burger_menu');
+
+const burgerCloseIcon = document.querySelector('.burger__close-icon');
 
 const toggleSearch = function () {
 	regionSearch.classList.toggle('hidden');
@@ -22,8 +27,35 @@ const toggleAgeArrow = function () {
    // regionSearch.
 };
 
+const openModal = function () {
+   modalBurgerMenu.classList.toggle('opened');
+}
+
+const closeModal = function () {
+   modalBurgerMenu.classList.toggle('opened');
+}
+
 regionSearch.addEventListener('click', toggleSearch);
 selectArrow.addEventListener('click', toggleSearch);
 
 selectSexArrow.addEventListener('click', toggleSexArrow);
 selectAgeArrow.addEventListener('click', toggleAgeArrow);
+
+
+burgerIcon.addEventListener('click', (event) => {
+   openModal();
+	// Исключаем прокрутку документа под открытым окном сообщения об ошибке
+	document.body.style.overflow = 'hidden';
+	// sectionError.classList.remove('hidden');
+	// sectionError.classList.remove('fade_left');
+	// sectionError.classList.add('appear_left');
+});
+
+burgerCloseIcon.addEventListener('click', (event) => {
+   closeModal();
+	// Исключаем прокрутку документа под открытым окном сообщения об ошибке
+	document.body.style.overflow = '';
+});
+
+// burgerIcon.addEventListener('click', openModal);
+// burgerCloseIcon.addEventListener('click', closeModal);
